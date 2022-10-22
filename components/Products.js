2646e8b4d449ccc8b7f10c4bc9ff1cf40e74fd54
tarek421 <tarekmahmud740@gmail.com>
2022-10-23 00:17:32 +0600

    product page compleate

---
+++ components/Products.js
@@ -0,0 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+
+const products = ({ product }) => {
+    const { slug, image, name, rating, brand, price } = product;
+    return (
+        <div className='card text-center'>
+            <Link href={`/product/${slug}`}>
+                <a>
+                    <img
+                        src={image}
+                        alt={slug}
+                        className='rounded-md shadow-inner shadow-md'
+                    />
+                </a>
+            </Link>
+            <div className='p-3'>
+                <Link href={`/product/${slug}`}>
+                    <h2 className='text-lg text-purple-600'>{name}</h2>
+                </Link>
+                <h3>{rating}</h3>
+                <h4 className=''>{brand}</h4>
+                <h3 className=''>{price}</h3>
+                <button className='primary-button'>
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default products;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import Products from '../components/Products';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <div>
       <Layout title="Home page">
-        <h1 className='text-3xl font-bold'>This project with TailwindCss and NextJs</h1>
+        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {
+            data.products.map(product => <Products product={product} key={product.slug} />)
+          }
+        </div>
       </Layout>
     </div>
   )
