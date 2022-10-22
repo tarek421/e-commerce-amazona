3180417b00bf0426a995446a6330b62126c82b45
tarek421 <tarekmahmud740@gmail.com>
2022-10-22 21:29:54 +0600

    add layout component and edit readme file

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import Head from 'next/head'
-import Layout from '../Components/Layout'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <div>
-      <Layout>
+      <Layout title="Home page">
         <h1 className='text-3xl font-bold'>This project with TailwindCss and NextJs</h1>
-
       </Layout>
-
-
     </div>
   )
 }
